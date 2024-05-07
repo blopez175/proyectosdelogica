068b803f216384d68b75e0de8956871ccd03974b
blopez175 <blopez@miumg.edu.gt>
2024-05-06 23:53:40 -0600

    se agregaron los archivos de hola mundo y variables

---
+++ variables.py
@@ -0,0 +1,43 @@
+'''# Calculo del área de un rectangulo base
+base=10
+altura=15
+area=base*altura
+perimetro=2*base+2*altura
+print('El área del rectangulo es ', area)
+print('El permietro del rectangulo es ', perimetro)
+base=0
+altura=0
+base=float(input('Ingrese la base del rectangulo '))
+altura=float(input('Ingrese la altura del rectangulo '))
+area=base*altura
+perimetro=2*base+2*altura
+print('El área del rectangulo es ', area)
+print('El permietro del rectangulo es ', perimetro)
+radio=0
+pi=3.1416
+radio=float(input('Ingrese el valor del radio '))
+area=pi*radio**2
+perimetro=2*pi*radio
+print('El area de tu circulo es ', area)
+print('El prerimetro de tu circulo es ', perimetro)
+a=0
+b=0
+a=float(input('Ingrese el valor del cateto #1 '))
+b=float(input('Ingrese el valor del cateto #2 '))
+c=(a*2 + b2)*(1/2)
+print('La hipotenusa de un triangulo es ', c)
+nombre=0
+edad=0
+anioDePersona=0
+fechaActual=2024
+fechaNacimiento=0
+nombre=input('Ingresa tu nombre ')
+edad=int(input('Ingresa tu edad '))
+fechaNacimiento=fechaActual-edad
+print('Hola ', nombre, ' El año en que naciste es ', fechaNacimiento)'''
+anionac=int(input('Ingrese su año de nacimiento '))
+edad=2024-anionac
+if edad>=18:
+    print('Es mayor de edad ')
+else:
+    print('Es menor de edad ')
